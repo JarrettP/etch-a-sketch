204fd034fae36970f78bc47d58edefaf81f6647d
Jarrett <contact@jarrettperkins.me>
2022-12-14 21:45:56 -0600

    updated grid to 16x16 and added hover

--- script.js
+++ script.js
@@ -1,37 +1,15 @@
 const container = document.getElementById('container');
-const fragment = document.createDocumentFragment();
 
-// Div Grid
-//   1 2 3 4
-// a x x x x
-// b x x x x
-// c x x x x
-// d x x x x
-const squares = [
-    "a1",
-    "a2",
-    "a3",
-    "a4",
-    "b1",
-    "b2",
-    "b3",
-    "b4",
-    "c1",
-    "c2",
-    "c3",
-    "c4",
-    "d1",
-    "d2",
-    "d3",
-    "d4"
-];
-
-squares.forEach((text) => {
+for (let i = 0; i < 256; i++) {
     const div = document.createElement('div');
     div.classList.add('item');
-    div.setAttribute('style',`grid-area: ${text};`)
-    div.textContent = text
-    fragment.appendChild(div);
-});
+    container.appendChild(div);
+}
 
-container.appendChild(fragment);
+container.onmouseover = (event) => {
+    event.target.classList.add('hover');
+};
+
+container.onmouseout = (event) => {
+    event.target.classList.remove('hover');
+};
