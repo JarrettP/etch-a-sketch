93b1218a31b4a67f423495f0a049ba1c7f1f5316
Jarrett <contact@jarrettperkins.me>
2022-12-16 11:28:37 -0600

    Clarified mouseover target for item class only. Removed borders

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const container = document.getElementById('container');
 
-let size = 10
+let size = 100
 
 container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 
-for (let i = 0; i < (size ** size); i++) {
+for (let i = 0; i < size * size; i++) {
     const div = document.createElement('div');
     div.classList.add('item');
     container.appendChild(div);
@@ -12,17 +12,14 @@ for (let i = 0; i < (size ** size); i++) {
 
 const square = container.querySelectorAll('div.item');
 
-square.forEach(() => {
-    onmouseover = (event) => {
+
+onmouseover = (event) => {
+    if (event.target.classList.contains('item')) {
         event.target.classList.add('hover');
-    };
-});
+    }    
+};
 
 
 // container.onmouseout = (event) => {
 //     event.target.classList.remove('hover');
 // };
-
-
-// NEED TO FIX ALL ELEMENTS ADDING .hover CLASS.
-// I ONLY WANT THE SQUARES TO DO THAT.
