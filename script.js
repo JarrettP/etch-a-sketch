c4a2e8526bd99638e51e33c6c5b25901144e0d2b
Jarrett <contact@jarrettperkins.me>
2022-12-19 22:12:11 -0600

    Added button to create new grid with size n

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
-const container = document.getElementById('container');
+const container = document.querySelector('#container');
+const sizeSelector = document.querySelector('#sizeSelector');
 
-let size = 100
+let size = 16;
 
-container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+sizeSelector.addEventListener('click', () => {
+    removeAllChildNodes(container);
+    
+    do {
+        size = prompt("Please input the side length of your grid.")
+    } while (size > 100);
+   
+    gridGenerator(size);
+});
 
-for (let i = 0; i < size * size; i++) {
-    const div = document.createElement('div');
-    div.classList.add('item');
-    container.appendChild(div);
-}
+function gridGenerator(size) {
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 
-const square = container.querySelectorAll('div.item');
+    for (let i = 0; i < size * size; i++) {
+        const div = document.createElement('div');
+        div.classList.add('item');
+        container.appendChild(div);
+    };
+};
 
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    };
+};
 
 onmouseover = (event) => {
     if (event.target.classList.contains('item')) {
         event.target.classList.add('hover');
     }    
 };
-
-
-// container.onmouseout = (event) => {
-//     event.target.classList.remove('hover');
-// };
